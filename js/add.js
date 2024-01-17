335fb6e1561eb387e794ffeeafc2c838c2578cfd
Adrian Sierra <asierra010@gmail.com>
2024-01-16 23:27:36 -0800

    added content to add-punch page

--- js/add.js
+++ js/add.js
@@ -1,7 +1,5 @@
 const nav_icon = document.getElementById("nav-icon");
 const links_element = document.getElementById("links-element");
-const time_element = document.getElementById("time");
-const date_element = document.getElementById("date");
 
 nav_icon.addEventListener("click", () => {
   if (nav_icon.dataset.icon === "open") {
@@ -12,15 +10,3 @@ nav_icon.addEventListener("click", () => {
     links_element.classList.add("hide");
   }
 });
-let currentDate, currentTime;
-function updateDateTime() {
-  const now = new Date();
-  // get the current date and time as a string
-  currentDate = now.toLocaleDateString();
-  currentTime = now.toLocaleTimeString();
-  date_element.textContent = currentDate;
-  time_element.textContent = currentTime;
-}
-// call the `updateDateTime` function every second
-updateDateTime();
-setInterval(updateDateTime, 1000);
