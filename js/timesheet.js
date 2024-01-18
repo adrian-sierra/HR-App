53c50cfcb9dab48db2a57faf8b3057623e3c0b9d
Adrian Sierra <asierra010@gmail.com>
2024-01-17 21:16:36 -0800

    added changed

--- js/timesheet.js
+++ js/timesheet.js
@@ -1,5 +1,6 @@
 const nav_icon = document.getElementById("nav-icon");
 const links_element = document.getElementById("links-element");
+const current_week = document.getElementById("entry-week");
 
 nav_icon.addEventListener("click", () => {
   if (nav_icon.dataset.icon === "open") {
@@ -10,3 +11,18 @@ nav_icon.addEventListener("click", () => {
     links_element.classList.add("hide");
   }
 });
+
+function getMondayOfCurrentWeek() {
+  let fullDate = "";
+  let d = new Date();
+  let starting_monday = d.getDate() - d.getDay() + 1;
+  return (
+    fullDate +
+    (d.getMonth() + 1) +
+    "/" +
+    starting_monday +
+    "/" +
+    d.getFullYear()
+  );
+}
+current_week.textContent = getMondayOfCurrentWeek();
