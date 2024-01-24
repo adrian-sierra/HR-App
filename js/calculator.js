cd02bfaa5090b6cbfd6744bb051e3ada4460c265
Adrian Sierra <asierra010@gmail.com>
2024-01-23 22:16:24 -0800

    added content and functionality to check calc

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,6 @@
 const nav_icon = document.getElementById("nav-icon");
 const links_element = document.getElementById("links-element");
+const calc_button = document.getElementById("calculation-button");
 
 nav_icon.addEventListener("click", () => {
   if (nav_icon.dataset.icon === "open") {
@@ -10,3 +11,26 @@ nav_icon.addEventListener("click", () => {
     links_element.classList.add("hide");
   }
 });
+
+const gross_result = document.getElementById("gross-number");
+const net_result = document.getElementById("net-number");
+function calcTotal() {
+  const check_inputs = document.querySelectorAll(".form-input");
+  let total_hours = parseFloat(check_inputs[0].value);
+  const total_ot = parseFloat(check_inputs[1].value);
+  const hourly_rate = parseFloat(check_inputs[2].value);
+  let gross_total = 0;
+  let net_total;
+  if (total_ot > 0) {
+    total_hours += total_ot * 1.5;
+    gross_total += total_hours * hourly_rate;
+  } else {
+    gross_total += total_hours * hourly_rate;
+  }
+  net_total = (gross_total * 0.72).toFixed(2);
+  gross_total = gross_total.toFixed(2);
+  gross_result.textContent = "$" + gross_total;
+  net_result.textContent = "$" + net_total;
+}
+
+calc_button.addEventListener("click", calcTotal);
