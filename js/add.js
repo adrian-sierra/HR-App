551297e5e0a55e091675bf2150321e24dea46555
Adrian Sierra <asierra010@gmail.com>
2024-02-02 01:00:19 -0800

    added edit button logic to add punch page

--- js/add.js
+++ js/add.js
@@ -10,3 +10,33 @@ nav_icon.addEventListener("click", () => {
     links_element.classList.add("hide");
   }
 });
+
+const edit_titles = document.querySelectorAll(".entry-titles");
+function removeTitles() {
+  for (let i = 0; i < edit_titles.length; i++) {
+    edit_titles[i].classList.add("hide");
+  }
+}
+const edit_entries = document.querySelectorAll(".entry");
+function removeButtons(idx) {
+  for (let i = 0; i < edit_buttons.length; i++) {
+    if (i != idx) {
+      edit_entries[i].classList.add("hide");
+    }
+  }
+}
+const form_date = document.getElementById("form-date");
+const form_type = document.getElementById("form-type");
+const form_time = document.getElementById("form-time");
+function insertData() {
+  form_date.valueAsDate = "1/4/2024";
+  form_type.value = "clock-out";
+}
+const edit_buttons = document.querySelectorAll(".edit-entry");
+for (let i = 0; i < edit_buttons.length; i++) {
+  edit_buttons[i].addEventListener("click", () => {
+    removeButtons(i);
+    removeTitles();
+    insertData();
+  });
+}
