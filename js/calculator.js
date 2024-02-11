1ae4daa7f094376e921a7cd1aeea76f3309c0cb5
Adrian Sierra <asierra010@gmail.com>
2024-02-10 16:14:46 -0800

    added error check and visual to calculator

--- js/calculator.js
+++ js/calculator.js
@@ -12,25 +12,42 @@ nav_icon.addEventListener("click", () => {
   }
 });
 
+function checkEmptyFields(inputs) {
+  let error = false;
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value == "") {
+      inputs[i].classList.add("error");
+      error = true;
+    } else {
+      if (inputs[i].classList.contains("error")) {
+        inputs[i].classList.remove("error");
+      }
+    }
+  }
+  return error;
+}
+
 const gross_result = document.getElementById("gross-number");
 const net_result = document.getElementById("net-number");
 function calcTotal() {
   const check_inputs = document.querySelectorAll(".form-input");
-  let total_hours = parseFloat(check_inputs[0].value);
-  const total_ot = parseFloat(check_inputs[1].value);
-  const hourly_rate = parseFloat(check_inputs[2].value);
-  let gross_total = 0;
-  let net_total;
-  if (total_ot > 0) {
-    total_hours += total_ot * 1.5;
-    gross_total += total_hours * hourly_rate;
-  } else {
-    gross_total += total_hours * hourly_rate;
+  if (!checkEmptyFields(check_inputs)) {
+    let total_hours = parseFloat(check_inputs[0].value);
+    const total_ot = parseFloat(check_inputs[1].value);
+    const hourly_rate = parseFloat(check_inputs[2].value);
+    let gross_total = 0;
+    let net_total;
+    if (total_ot > 0) {
+      total_hours += total_ot * 1.5;
+      gross_total += total_hours * hourly_rate;
+    } else {
+      gross_total += total_hours * hourly_rate;
+    }
+    net_total = (gross_total * 0.72).toFixed(2);
+    gross_total = gross_total.toFixed(2);
+    gross_result.textContent = "$" + gross_total;
+    net_result.textContent = "$" + net_total;
   }
-  net_total = (gross_total * 0.72).toFixed(2);
-  gross_total = gross_total.toFixed(2);
-  gross_result.textContent = "$" + gross_total;
-  net_result.textContent = "$" + net_total;
 }
 
 calc_button.addEventListener("click", calcTotal);
